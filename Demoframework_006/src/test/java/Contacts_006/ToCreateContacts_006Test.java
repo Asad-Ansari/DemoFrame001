6ccdcd485b71288561e60f1ba7f7ba7af95484ef
HP <HP@DESKTOP-N19O9LD>
2024-11-05 09:26:15 +0530

    Push 2

---
+++ Demoframework_006/src/test/java/Contacts_006/ToCreateContacts_006Test.java
@@ -0,0 +1,10 @@
+package Contacts_006;
+
+public class ToCreateContacts_006Test {
+
+	public static void main(String[] args) {
+		System.out.println("Contacts");
+
+	}
+
+}
---
+++ Demoframework_006/src/test/java/Organization_006/ToCreateOrganization_006Test.java
@@ -0,0 +1,10 @@
+package Organization_006;
+
+public class ToCreateOrganization_006Test {
+
+	public static void main(String[] args) {
+		System.out.println("Organization");
+
+	}
+
+}
